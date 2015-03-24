977372252340b047a01763b35230abcbb90f1706
Pragya Bajoria <pbajoria@umass.edu>
2015-03-24 09:33:17 -0400

    add profile page

--- app.js
+++ app.js
@@ -180,6 +180,20 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
+app.get('/profile', function (req, res) {
+  // Create the form to add a new user
+  var form = '<p><b>Add Profile Information</b></p><br>' +      
+    '<form action="/users/add" method="get">' +
+    'UID:<br> <input type="text" name="uid"><br><br>' + 
+    'First Name:<br> <input type="text" name="fname"><br><br>' +
+    'Last Name:<br> <input type="text" name="lname"><br><br>' +
+    'University:<br> <input type="text" name="university"><br><br>' +
+    'Age:<br> <input type="text" name="age"><br><br>' +
+    '<input type="submit" value="Submit Profile Changes">' +
+    '</form><br><br>';
+  res.send(form);
+});
+
 
 //mysql connection
 var connection  = require('express-myconnection'),
