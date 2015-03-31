482abc6cde6e5a2db655027023ce81929fdbe0f9
aziz22q <aziz22q@mtholyoke.edu>
2015-03-31 01:21:01 -0400

    changed database

--- mhcrideshare.sql
+++ mhcrideshare.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `mhcrideshare` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `mhcrideshare`;
 -- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
 --
 -- Host: localhost    Database: mhcrideshare
@@ -18,6 +16,34 @@ USE `mhcrideshare`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `locations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `zipcode` varchar(45) DEFAULT NULL,
+  `lastrequestedridedate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES (1,'Smith College','Northampton','MA','00770','0000-00-00 00:00:00'),(3,'Amherst College','Amherst','MA','00770','0000-00-00 00:00:00');
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `members`
 --
 
@@ -31,8 +57,12 @@ CREATE TABLE `members` (
   `email` varchar(45) DEFAULT NULL,
   `phone` varchar(45) DEFAULT NULL,
   `status` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `facebookid` varchar(45) DEFAULT NULL,
+  `gmailid` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gmailid_UNIQUE` (`gmailid`),
+  UNIQUE KEY `facebookid_UNIQUE` (`facebookid`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +71,7 @@ CREATE TABLE `members` (
 
 LOCK TABLES `members` WRITE;
 /*!40000 ALTER TABLE `members` DISABLE KEYS */;
-INSERT INTO `members` VALUES (1,'Jane','Smith','lisa@example.com','413000000','active'),(2,'Alice','Roberts','alice@example.com','4131110001','active'),(3,'Fatima','Reza','fatima@example.com','413888999','active'),(4,'Anna','Jones','anna@example.com','67288878787','inactive'),(5,'Ellen','Johnson','ellen@example.com','87827481278','active');
+INSERT INTO `members` VALUES (1,'Jane','Smith','lisa@example.com','4130000004','active',NULL,NULL),(2,'Alice','Roberts','alice@example.com','4131110001','active',NULL,NULL),(3,'Fatima','Reza','fatima@example.com','413888999','active',NULL,NULL),(4,'Anna','Jones','anna@example.com','67288878787','inactive',NULL,NULL),(5,'Ellen','Johnson','ellen@example.com','87827481278','active',NULL,NULL),(10,'AAAA','BBBB','ab@example.com','863861868','XYZ',NULL,NULL);
 /*!40000 ALTER TABLE `members` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,15 +107,17 @@ DROP TABLE IF EXISTS `rides`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rides` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `driverid` int(11) DEFAULT NULL,
-  `origin` varchar(45) DEFAULT NULL,
-  `destination` varchar(45) DEFAULT NULL,
+  `origin` int(11) DEFAULT NULL,
+  `destination` int(11) DEFAULT NULL,
   `seats` int(11) DEFAULT NULL,
   `flexibility` int(11) DEFAULT NULL,
   `datetime` datetime DEFAULT NULL,
+  `offered` varchar(45) DEFAULT NULL,
+  `requested` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,7 +126,7 @@ CREATE TABLE `rides` (
 
 LOCK TABLES `rides` WRITE;
 /*!40000 ALTER TABLE `rides` DISABLE KEYS */;
-INSERT INTO `rides` VALUES (1,2,'Mt. Holyoke','Boston',1,2,'2015-03-12 03:00:00'),(2,2,'Boston','Mt. Holyoke',1,2,'2015-03-22 03:00:00');
+INSERT INTO `rides` VALUES (1,2,1,3,1,2,'2015-03-12 03:00:00',NULL,NULL),(2,2,3,1,1,2,'2015-03-22 03:00:00',NULL,NULL),(4,1,1,1,1,1,'2018-01-19 03:14:07',NULL,NULL);
 /*!40000 ALTER TABLE `rides` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -107,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-03-06  2:49:37
+-- Dump completed on 2015-03-31  1:20:08
